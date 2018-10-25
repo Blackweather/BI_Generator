3d7a2ff185af5c821f0681c8f22f761e48407c0c
kszczepa <karol.szczepanski@intel.com>
2018-10-25 14:48:42 +0200

    Initial Commit

---
+++ BI_Generator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: add parameter parsing
+
+            // TODO: generate data for RDB
+
+            // TODO: generate data for EXCEL sheet
+        }
+    }
+}

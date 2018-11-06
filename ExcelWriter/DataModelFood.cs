d43832a9efda62aad5313e3c06e6d0151fe94a3a
Karol Szczepański <szczepanski.karol0@gmail.com>
2018-11-06 10:38:33 +0100

    Corrected EXCEL data models

---
+++ ExcelWriter/DataModelFood.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWriter {
+    class DataModelFood {
+        // excel data model for sheet 2 for food orders
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ExcelWriter/DataModelSales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWriter {
+    class DataModelSales {
+        // excel data model for sheet 1 for ticket sales
+        private DateTime _date;
+        private int _ticketsSold;
+        private int _ticketsSoldChildren;
+        private int _ticketsSoldAdults;
+    }
+}
